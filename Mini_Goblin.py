50d48f5548351332c939a63e68d1d6d99f527af1
Kian Najmechi <knajmechi@gmail.com>
2025-03-27 11:12:34 +0100

    permanent version of mini_goblin

--- Mini_Goblin.py
+++ Mini_Goblin.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from pathlib import Path
 import sqlite3
 #from mini_storage import mini_storage  # Import the instance
-from mini_storage import mini_storage, MiniStorage, __init__
+from mini_storage import mini_storage
 import logging
 import asyncio  
 
